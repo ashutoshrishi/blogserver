29a72930eb9c86f5607adb8b0e9bcc8f72bc8d7c
rishi <ashutoshrishi92@gmail.com>
2016-08-14 14:19:03 +1000

    Boilerplate for DB access and GET API endpoints

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,101 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Control.Monad.IO.Class (liftIO)
+import           Data.Aeson (toJSON, object, (.=), Value (Null))
+import           Data.Monoid (mconcat)
+import           Data.Text
+import qualified Database.Persist as DB
+import qualified Database.Persist.Postgresql as DB
+import           Model
+import           Network.Wai (Middleware)
+import           Network.Wai.Handler.Warp (defaultSettings)
+import           Network.Wai.Middleware.RequestLogger (logStdout, logStdoutDev)
+import Network.HTTP.Types.Status (notFound404)
+import           System.Environment (lookupEnv)
+import           Web.Scotty
 
-main :: IO ()
-main = putStrLn "Hello, World!"
+
+main :: IO () 
+main = getConfig >>= runServer
+
+
+-- * Configuration
+
+data Config = Config
+    { environment :: Environment }
+
+data Environment = Development | Production
+    deriving (Show, Eq, Read)
+
+getConfig :: IO Config 
+getConfig = getEnvironment >>= return . Config
+    
+
+getEnvironment :: IO Environment
+getEnvironment = (maybe Development read) <$> lookupEnv "SCOTTY_ENV"
+        
+-- | Derive Scotty options from the server configuration
+getOptions :: Environment -> IO Options
+getOptions e = do
+    let v = case e of
+                Development -> 1
+                Production -> 0
+    -- For now we are going with default Warp settings
+    return $ Options v defaultSettings
+
+
+
+-- | Logging middleware.
+loggingM :: Environment -> Middleware
+loggingM Development = logStdoutDev
+loggingM Production = logStdout
+
+
+-- * Main runners
+
+runServer :: Config -> IO ()
+runServer c = do
+    let env = environment c
+    opts <- getOptions env
+    scottyOpts opts (application env)
+
+
+application :: Environment -> ScottyM ()
+application env = do
+    middleware (loggingM env)
+    get "/" (text "Hello, World!")
+    get "/posts" getPostsA
+    get "/post/slug/:slug" getPostBySlugA
+    get "/post/id/:id" getPostByIdA
+    notFound notFoundA
+
+
+-- * Actions
+
+-- | Action for route "/posts".
+getPostsA :: ActionM ()
+getPostsA = do
+    ens <- liftIO $ getNPosts 10
+    json $ object ["posts" .= ens]
+
+
+getPostBySlugA :: ActionM ()
+getPostBySlugA = do
+    slug <- param "slug"
+    maybeEnt <- liftIO $ getPostBySlug slug
+    maybe notFoundA json maybeEnt
+
+
+getPostByIdA :: ActionM ()
+getPostByIdA = do
+    id <- param "id"
+    maybeEnt <- liftIO $ getPostById (read id :: Integer)
+    maybe notFoundA json maybeEnt
+    
+
+
+notFoundA :: ActionM ()
+notFoundA = do
+    status notFound404
+    json Null 
---
+++ src/Model.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+
+module Model (
+    Post(..), runDb, doMigration,
+    -- * interface
+    insertPost, getNPosts, getPostBySlug, getPostById
+    ) where
+
+import Database.Persist
+import Database.Persist.TH
+import Database.Persist.Sqlite
+import Database.Persist.Postgresql
+
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Logger    (runStderrLoggingT)
+
+import Data.Text
+import Data.Time.Clock (UTCTime)
+import qualified Data.List as List
+
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Post json
+    title Text
+    content Text
+    slug String
+    created UTCTime default=now()
+    UniqueSlug slug
+    deriving Show
+|]
+    
+
+runDb :: SqlPersistM a -> IO a
+runDb query = do
+    let connStr = "host=localhost user=rishi dbname=rishi" 
+    runStderrLoggingT $ withPostgresqlPool connStr 10 $ \pool ->
+        liftIO $ runSqlPersistMPool query pool
+
+
+doMigration :: IO ()
+doMigration = runDb (runMigration migrateAll)
+
+-- INTERFACE
+
+insertPost :: Post -> IO (Key Post)
+insertPost post = runDb (insert post)
+
+
+getNPosts :: Int -> IO [Entity Post]
+getNPosts n = runDb $
+    selectList ([] :: [Filter Post]) [LimitTo n]
+
+
+getPostBySlug :: String -> IO (Maybe (Entity Post))
+getPostBySlug s = runDb $ getBy $ UniqueSlug s
+
+
+getPostById :: Integer -> IO (Maybe (Entity Post))
+getPostById i = runDb $ do
+    posts <- selectList [PostId ==. toKey i] []
+    if List.null posts
+    then return Nothing
+    else return $ Just (List.head posts)
+
+
+-- * Utilities
+toKey :: ToBackendKey SqlBackend a => Integer -> Key a
+toKey i = toSqlKey (fromIntegral (i :: Integer))
+
+-- main :: IO ()
+-- main = runDb $ do
+--     time <- liftIO getCurrentTime
+--     postId <- insert $ Post "Bar" "Second Random content" "bar" time 
+--     post <- get postId
+--     liftIO $ print post    
+
+    
+    
