7b5f0935860073c1f17a87a5e6483e946910bc27
rishi <ashutoshrishi92@gmail.com>
2016-08-24 22:28:44 +1000

    Added config file for listing post sources

--- src/Types.hs
+++ src/Types.hs
@@ -1,22 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Types
-    ( -- * Server monad
-        Server, ServerState(..)
-    , Environment(..), getEnvironment, getOptions
+    ( Server, ServerState(..)
+    -- * Configuration types
+    , Environment(..), getEnvironment, getOptions, Source(..)
+    , getConfig
+    , logMsg
+
     ) where
 
-
-import Control.Monad.Trans.State.Lazy (StateT)
+import Control.Monad.IO.Class (liftIO)
+import Control.Applicative (empty)
+import Control.Monad.Trans.State.Lazy (StateT, modify)
 import Network.Wai.Handler.Warp (defaultSettings)
 import System.Environment (lookupEnv)
 import Web.Scotty (Options(..))
+import GHC.Generics
+import Data.Aeson
+import Data.Aeson.Types (typeMismatch)
+import qualified Data.ByteString.Lazy as BL
 
 
 -- |State monad holding the configuration of a running server.
 data ServerState = ServerState
-    { environment :: Environment }
+    { environment :: Environment
+    , postSources :: [Source] }
+
 
 -- |Hold the server state over the standard IO monad.
 type Server = StateT ServerState IO
+
 
 
 -- |Represents different environment variables that can be set.
@@ -38,3 +51,52 @@ getOptions e = do
                 Production -> 0
     -- For now we are going with default Warp settings
     return $ Options v defaultSettings
+
+
+-- |All sources that can contain markdown posts.
+data Source = Disk FilePath
+
+
+instance Show Source where
+    show (Disk f) = "DIR: " ++ f
+
+
+data ConfigFile = ConfigFile
+    { sources :: [Source] }
+
+
+instance FromJSON ConfigFile where
+    parseJSON (Object v) = do
+        ss <- v .: "sources"
+        diskSources <- ss .:? "disk" .!= ([] :: [FilePath])
+        return $ ConfigFile (Disk <$> diskSources)
+
+    parseJSON c = typeMismatch "ConfigFile" c
+
+
+-----------------------------------------------------------------------------
+-- Config Parsing                                                          --
+-----------------------------------------------------------------------------
+
+
+getConfig :: FilePath -> Server ()
+getConfig f = do
+    maybeConf <- parseConfigFile f
+    case maybeConf of
+        -- update config
+        Just conf -> do
+            liftIO $ putStrLn $ "Adding sources: " ++ show (sources conf)
+            modify (\s -> s { postSources = sources conf ++ postSources s })
+        -- keep config
+        Nothing -> return ()
+
+
+
+parseConfigFile :: FilePath -> Server (Maybe ConfigFile)
+parseConfigFile f = do
+    confStr <- liftIO $ BL.readFile f
+    return $ decode confStr
+
+
+logMsg :: String -> Server ()
+logMsg = liftIO . putStrLn
